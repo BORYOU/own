e6a246a9061a50853fb70d97617ec2079dd7a267
BORYOU <aa2216@163.com>
2017-05-25 00:22:32 +0800

    .

--- 生成W/addshelter.m
+++ 生成W/addshelter.m
@@ -1,45 +1,47 @@
-function addshelter(database, percent, shelter)
+function outfilename = addshelter(database, percent, shelter)
+% add shelter on database in percent, out file name is data_shelter_shelter_percent_percent.mat
 % database:
 %           'Y': 'YaleB_c.mat'
 %           'O': 'Orl.mat'
 % percent: 87 means 87%
 % shelter: 20 means 20 * 20 shelter
-    if database == 'Y'
-        load('YaleB_c.mat','A','Y');
-    elseif database == 'O'
-        load('Orl.mat')
-        %载入数据：A：原始数据矩阵，10304*400；Y：分类下标，400*1，一个人有十张脸部图片,图片的尺寸为 112*92
-    end
 
+    if database == 'Y'
+        data = 'YaleB_c';
+        %载入数据：A：原始数据矩阵，1024*2414；Y：分类下标，1*2414，一个人有越64张脸部图片,图片的尺寸为 32*32
+        facem = 32;
+        facen = 32;
+    elseif database == 'O'
+        data = 'Orl';
+        %载入数据：A：原始数据矩阵，10304*400；Y：分类下标，1*400，一个人有十张脸部图片,图片的尺寸为 112*92
+        facem = 112;
+        facen = 92;
+    end
+    load([data, '.mat'])
     [M,N] = size(A);
-    picNum = size(Y);
-    personNum = size(unique(Y));
-    A_new = A;
-    for i = 1 : personNum
-        
-        if mod(i,10) < 3
-            continue;
+    picNum = size(Y, 2);
+    peopleNum = size(unique(Y), 2);
+    for i = 1 : peopleNum
+        % get pic coll number of this person
+        y = find(Y==i);
+        firstInd = min(y);
+        lastInd = max(y);
+        personNum = lastInd - firstInd + 1;
+        % calculate shelter coll number
+        shelterNum = round((personNum * percent / 100))
+        addList = randperm(personNum, shelterNum) + (firstInd - 1);
+        for coll = addList
+            a = A(:,coll);
+            face = reshape(a,facem,facen);
+            lm = round((facem-shelter)*rand)+1;
+            ln = round((facen-shelter)*rand)+1;
+            face(lm:lm+shelter-1,ln:ln+shelter-1) = 0;
+            %imagesc(face);
+            %colormap('gray')
+            %pause(1)
+            a = reshape(face,M,1);
+            A(:,coll) = a/sum(a);
         end
-        a = A(:,i);
-        face = reshape(a,112,92);
-        lm = round(80*rand)+1;
-        ln = round(50*rand)+1;
-        face(lm:lm+29,ln:ln+39) = 0;
-        a = reshape(face,10304,1);
-        A_new(:,i) = a/sum(a);
     end
-    for i=1:2414
-    %    if mod(i,10) < 3
-    %        continue;
-    %    end
-        a = A(:,i);
-        face = reshape(a,32,32);
-        lm = round(15*rand)+1;
-        ln = round(15*rand)+1;
-        face(lm:lm+14,ln:ln+14) = 0;
-    %    imagesc(face);colormap(gray);
-        a = reshape(face,1024,1);
-        A_new(:,i) = a/sum(a);
-    end
-    
-    save('YaleB_c_shelter','A','Y');
+    outfilename = [data,'_shelter_',num2str(shelter),'_percent_',num2str(percent),'.mat'];
+    save(outfilename,'A','Y');
