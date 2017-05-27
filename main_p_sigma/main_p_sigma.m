41b09543360f4d7ed08d27a19417662315b835dd
boryou <aa2216@163.com>
2017-05-28 00:56:02 +0800

    .

--- main_p_sigma/main_p_sigma.m
+++ main_p_sigma/main_p_sigma.m
@@ -1,36 +1,52 @@
 clc; clear all
 
-load(fullfile('..','generateW','Orl_shelter_30_30.mat')); 
-data = 'Orl_shelter_30_30';
+for data = {
+    'Orl_shelter_40_40';
+    'Orl_shelter_50_50';
+    'Orl_shelter_20_percent_20';
+    'YaleB_c';
+    'YaleB_c_shelter_10_10';
+    'YaleB_c_shelter_10_percent_20';
+}
+data = data{1};
+fprintf('%s\n',data);
 
+load(fullfile('..','generateW',[data,'.mat'])); 
+
+if data(1) == 'O'
 k = 100; gamma = 1e-8; a = 9;
+elseif data(1) == 'Y'
+k = 130; gamma = 6e-8; a = 7;
+end
+
+maxiter =200; tol = 1e-17; timelimit = 1000000;
+fold = 3; 
 
 [M,N] = size(A); % M*N Îª¾ØÕóAµÄÎ¬Êý
 % rng('default')
 randn('state',1);
 Winit = abs(randn(M,k)); Hinit = abs(randn(k,N));
 
-maxiter =200; tol = 1e-17; timelimit = 1000000;
-fold = 3; 
+if data(1) == 'O'
+pall = 2:10; sigmaall = [sqrt(2),sqrt(10)];
+elseif data(1) == 'Y'
+pall = 2:10; sigmaall = [sqrt(2),sqrt(5),sqrt(10),sqrt(15),sqrt(20)];
+end
+% sigmaall = [sqrt(2),sqrt(5),sqrt(10),sqrt(15),sqrt(20)];
 
+HGAall = zeros(length(pall),length(sigmaall));
+HGdAall = zeros(length(pall),length(sigmaall));
 % outDirName = 'Orl_shelter_30_30_p_2_10_sigma_2_5_10_15_20';
 % outDirName = 'Orl_shelter_40_40_p_2_10_sigma_2_10';
-outDirName = 'Orl_shelter_50_50_p_2_10_sigma_2_10';
+outDirName = [data, '_p_',num2str(pall(1)),'_',num2str(pall(end)),'_sigma_',num2str(sigmaall(1)),'_',num2str(sigmaall(end))];
 
 if exist(outDirName,'dir') ~= 7
     mkdir(outDirName)
 end
 
-HGAall = zeros(9,5);
-HGdAall = zeros(9,5);
-
-pall = 2:10;
-% sigmaall = [sqrt(2),sqrt(5),sqrt(10),sqrt(15),sqrt(20)];
-sigmaall = [sqrt(2),sqrt(10)];
-    
 tic
-for i = 1:5
-    for j = 1:2
+for i = 1:length(pall)
+    for j = 1:length(sigmaall)
 
         p = pall(i)
         sigma = sigmaall(j)
@@ -52,12 +68,13 @@ for i = 1:5
         AccuracyG_ASCG=HGA.acc;
         HGdA = Accuracy(3,A,Y,HGd);
         AccuracyGd_ASCG=HGdA.acc;
-%         save(fullfile(outDirName,['Orl_shelter_30_30','_p_',num2str(p),'_sigma_',num2str(sigma),'_acc.mat']),'HGA','HGdA');
-        save(fullfile(outDirName,['Orl_shelter_40_40','_p_',num2str(p),'_sigma_',num2str(sigma),'_acc.mat']),'HGA','HGdA');
+%        save(fullfile(outDirName,['Orl_shelter_30_30','_p_',num2str(p),'_sigma_',num2str(sigma),'_acc.mat']),'HGA','HGdA');
+        save(fullfile(outDirName,[data,'_p_',num2str(p),'_sigma_',num2str(sigma),'_acc.mat']),'HGA','HGdA');
         HGAall(i,j) = AccuracyG_ASCG;
         HGdAall(i,j) = AccuracyGd_ASCG;
     end
 end
 
-save(fullfile(outDirName,'Orl_shelter_40_40all.mat'),'HGAall','HGdAall');
+save(fullfile(outDirName,[data,'all.mat']),'HGAall','HGdAall');
+end
 toc
