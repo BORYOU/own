cd21ec4517d6237114ddc36f968a41235b382eaf
boryou <aa2216@163.com>
2017-06-06 22:59:23 +0800

    .

--- highperformance/addMission.py
+++ highperformance/addMission.py
@@ -183,8 +183,8 @@ def start(jession, jobnamebase, pattern):
         time.sleep(sleeptime)
     
 def main():
-    jobnamebase = "main_all_best_YaleB_shelter_10_percent_20_{}pool"
-    pattern = "/home/14121553/workspace/GNMFO17_5_29/YaleB_shelter_10_percent_20_sigma_sqrt10"
+    jobnamebase = "main_all_best_sORL40_percent_40_{}pool"
+    pattern = "/home/14121553/workspace/GNMFO17_5_29/Orl_shelter_40_percent_40_sigma_sqrt10"
     _UserAgents = ["Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:48.0) Gecko/20100101 Firefox/48.0"
         ]
